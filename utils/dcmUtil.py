5ff20b1bf95069f031679047e1b37071ba13428e
Jihwan <cuchoco@naver.com>
2022-05-13 06:35:31 +0000

    GradCam

--- utils/dcmUtil.py
+++ utils/dcmUtil.py
@@ -1,8 +1,10 @@
 import SimpleITK as sitk
 import numpy as np
+import torch
+import matplotlib.pyplot as plt
 
 # input: dicom file path
-def get_array(dicom_path : str) -> np.array : 
+def getArray(dicom_path : str) -> np.array : 
     return sitk.GetArrayFromImage(sitk.ReadImage(dicom_path)).squeeze()
 
 
@@ -12,3 +14,16 @@ def windowing(image: np.array, window_center: int, window_width: int) -> np.arra
     
     image = ((np.clip(image, lower, upper) - lower) / window_width) * 255
     return np.array(image, dtype = np.uint8)
+
+
+def tensorShow(arr:torch.tensor):
+    assert arr.ndim == 3
+    arr = arr.permute(1,2,0)
+    
+    if arr.shape[2] == 1:
+        plt.imshow(arr, cmap='gray')
+    else:
+        plt.imshow(arr)
+
+    plt.axis('off')
+    plt.show()
